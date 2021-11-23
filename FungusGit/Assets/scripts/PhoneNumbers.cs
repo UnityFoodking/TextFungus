5814326c0981f58ddc14afac88976d2ca52969ef
Vladimir99users <51012754+Vladimir99users@users.noreply.github.com>
2021-11-24 00:16:00 +1000

    Сделан Ui Phone и добавлен минимальный функционал
    Добавлено:
    1) UI Phone
    2) Сделан скрипт
    3) Реализован интерфейс для ошибок, определяет введён ли правильно номер(нужно ли это вообще?)

--- FungusGit/Assets/scripts/PhoneNumbers.cs
+++ FungusGit/Assets/scripts/PhoneNumbers.cs
@@ -3,18 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PhoneNumbers : MonoBehaviour
+public class PhoneNumbers : MonoBehaviour, IError
 {
-    public InputField _inputfield;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private InputField _inputfield;
+     private int countersNumbers = 0;
+   private int lenghtNumber = 9; //длина нашего номера пример: 555-628-59
 
-    // Update is called once per frame
-    void Update()
+    public string ErrorText => "Номер не определён";
+
+    public void InputNumbers(int valueNumber){
+       if(_inputfield.text.Length <= lenghtNumber){
+        _inputfield.text += valueNumber;
+         countersNumbers++;
+            if(countersNumbers % 3 == 0 )
+            {
+                _inputfield.text += "-";
+                countersNumbers = 0;
+            }
+        } 
+   }
+
+   public void ClearInputNumbers(){
+       _inputfield.text = "";
+       countersNumbers = 0;
+   }
+
+    public void ErrorMessage()
     {
-        
+        _inputfield.text = ErrorText;
     }
 }
